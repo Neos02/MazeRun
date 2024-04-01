4407f35268510726673431fc2f5d1a5828f88148
Nicholas Kennedy <nmk10062@gmail.com>
2024-04-01 14:19:17 -0400

    Add doxygen comments

--- drawing.js
+++ drawing.js
@@ -1,3 +1,8 @@
+/**
+ * Initializes canvas properties and scales it to the proper resolution.
+ * @param {Number} width the width of the canvas
+ * @param {Number} height the height of the canvas
+ */
 function initCanvas(width, height) {
   // Translate by half a pixel to remove blurriness
   ctx.translate(0.5, 0.5);
@@ -10,11 +15,26 @@ function initCanvas(width, height) {
   ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
 }
 
+/**
+ * Draws a filled in rectangle with the provided properties.
+ * @param {Number} x the x position of the rectangle
+ * @param {Number} y the y position of the rectangle
+ * @param {Number} width the width of the rectangle
+ * @param {Number} height the height of the rectangle
+ * @param {String} color the fill color
+ */
 function rect(x, y, width, height, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, width, height);
 }
 
+/**
+ * Draws a filled in circle with the provided properties.
+ * @param {Number} x the x position of the circle
+ * @param {Number} y the y position of the circle
+ * @param {Number} radius the radius of the circle
+ * @param {String} color the fill color
+ */
 function circle(x, y, radius, color) {
   ctx.fillStyle = color;
   ctx.beginPath();
--- game.js
+++ game.js
@@ -1,7 +1,19 @@
+/**
+ * Responsible for updating the game
+ * @param {Number} deltaTime the time in milliseconds since the last frame
+ */
 function update(deltaTime) {}
 
+/**
+ * Responsible for drawing items to the screen
+ */
 function draw() {}
 
+/**
+ * The main gameloop. Computes the deltatime and passes it to the update function.
+ * Calls draw and update.
+ * @param {Date} timestamp the current timestamp
+ */
 function loop(timestamp) {
   const deltaTime = timestamp - prevTime;
 
@@ -17,10 +29,12 @@ function loop(timestamp) {
   window.requestAnimationFrame(loop);
 }
 
+/**
+ * Computes and displays the FPS
+ * @param {Number} deltaTime the time in milliseconds since the last frame
+ */
 function showFps(deltaTime) {
-  const fps = Math.round(1000 / deltaTime);
-
   ctx.fillStyle = "black";
   ctx.fontWeight = 20;
-  ctx.fillText(`FPS: ${fps}`, 0, 10);
+  ctx.fillText(`FPS: ${Math.round(1000 / deltaTime)}`, 0, 10);
 }
